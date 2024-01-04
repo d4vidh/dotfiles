e8236f7f5b287ec629a96dd646623d0c0542ef3a
Daeyoung Hong <daeyoungh923@gmail.com>
2024-01-05 00:05:43 +0900

    feat: add steps to install asdf and its plugins

---
+++ home/.chezmoitemplates/asdf-lib.sh
@@ -0,0 +1,17 @@
+# shellcheck shell=bash
+
+function is_asdf_plugin_installed() {
+  local _plugin="$1"
+
+  asdf plugin list | grep "${_plugin}" >/dev/null
+}
+
+function is_asdf_package_installed() {
+  local _plugin="$1"
+  local _version="$2"
+
+  asdf list "${_plugin}" | grep "${_version}" >/dev/null
+}
+
+# shellcheck disable=SC2034
+readonly asdf_prefix="${HOME}/.asdf"
